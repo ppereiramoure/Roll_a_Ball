e91a0dd9f8a5d1a078ade3116e4e42f73a4d4648
Pablo <72071685+ppereiramoure@users.noreply.github.com>
2022-03-30 15:55:59 +0200

    commit 1

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent pathfinder;
+    private Transform target;
+
+    void Start()
+    {
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = GameObject.Find("Player").transform;
+    }
+    void Update()
+    {
+        pathfinder.SetDestination(target.position);
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+       private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // para el teclado
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+
+        // recogemos los datos del acelerometro
+        Vector3 dir = Vector3.zero;
+        dir.x = -Input.acceleration.y;
+        dir.z = Input.acceleration.x;
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        dir *= Time.deltaTime;
+        transform.Translate(dir * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        }
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            if (count > 0)
+            {
+                count--;
+                SetCountText();
+            }
+            other.gameObject.transform.position = new Vector3(9, 1, 9);
+            gameObject.transform.position = new Vector3(0, 1, 0);
+        }
+    }
+}
